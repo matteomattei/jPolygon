a20cdb360153dc97875e8c1ee4d6a7f0c551ec96
Matteo Mattei <matteo.mattei@gmail.com>
2015-09-13 12:43:31 +0200

    initial import

---
+++ README.md
@@ -0,0 +1,33 @@
+jPolygon is a javascript library that allows drawing a polygon in a HTML5 canvas over an image.
+It supports undo and clear funcions. To finalize the polygon press **CTRL + mouse Click**.
+
+Technologies used:
+------------------
+
+ - pure javascript (no jQuery, etc...)
+ - HTML5 canvas
+
+Requirements:
+-------------
+
+ - Any browser that supports HTML5 canvas
+
+Main Features:
+--------------
+
+ - Mark points with a square.
+ - Undo function implementation.
+ - Clear function implementation.
+ - When a polygon is created, fill the content with a transparent color.
+
+License:
+--------
+
+ - MIT license
+
+URL:
+----
+
+ - Project sources are hosted on [Github](https://github.com/matteomattei/jPolygon)
+ - A live demo is available [here](http://www.matteomattei.com/projects/jpolygon/example.html)
+
---
+++ jPolygon.js
@@ -0,0 +1,148 @@
+var perimeter = new Array();
+var complete = false;
+var canvas = document.getElementById("jPolygon");
+var ctx;
+
+function line_intersects(p0, p1, p2, p3) {
+    var s1_x, s1_y, s2_x, s2_y;
+    s1_x = p1['x'] - p0['x'];
+    s1_y = p1['y'] - p0['y'];
+    s2_x = p3['x'] - p2['x'];
+    s2_y = p3['y'] - p2['y'];
+
+    var s, t;
+    s = (-s1_y * (p0['x'] - p2['x']) + s1_x * (p0['y'] - p2['y'])) / (-s2_x * s1_y + s1_x * s2_y);
+    t = ( s2_x * (p0['y'] - p2['y']) - s2_y * (p0['x'] - p2['x'])) / (-s2_x * s1_y + s1_x * s2_y);
+
+    if (s >= 0 && s <= 1 && t >= 0 && t <= 1)
+    {
+        // Collision detected
+        return true;
+    }
+    return false; // No collision
+}
+
+function point(x, y){
+    ctx.fillStyle="white";
+    ctx.strokeStyle = "white";
+    ctx.fillRect(x-2,y-2,4,4);
+    ctx.moveTo(x,y);
+}
+
+function undo(){
+    ctx = undefined;
+    perimeter.pop();
+    complete = false;
+    start(true);
+}
+
+function clear_canvas(){
+    ctx = undefined;
+    perimeter = new Array();
+    complete = false;
+    document.getElementById('coordinates').value = '';
+    start();
+}
+
+function draw(end){
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = "white";
+    ctx.lineCap = "square";
+    ctx.beginPath();
+
+    for(var i=0; i<perimeter.length; i++){
+        if(i==0){
+            ctx.moveTo(perimeter[i]['x'],perimeter[i]['y']);
+            end || point(perimeter[i]['x'],perimeter[i]['y']);
+        } else {
+            ctx.lineTo(perimeter[i]['x'],perimeter[i]['y']);
+            end || point(perimeter[i]['x'],perimeter[i]['y']);
+        }
+    }
+    if(end){
+        ctx.lineTo(perimeter[0]['x'],perimeter[0]['y']);
+        ctx.closePath();
+        ctx.fillStyle = 'rgba(255, 0, 0, 0.5)';
+        ctx.fill();
+        ctx.strokeStyle = 'blue';
+        complete = true;
+    }
+    ctx.stroke();
+
+    // print coordinates
+    if(perimeter.length == 0){
+        document.getElementById('coordinates').value = '';
+    } else {
+        document.getElementById('coordinates').value = JSON.stringify(perimeter);
+    }
+}
+
+function check_intersect(x,y){
+    if(perimeter.length < 4){
+        return false;
+    }
+    var p0 = new Array();
+    var p1 = new Array();
+    var p2 = new Array();
+    var p3 = new Array();
+
+    p2['x'] = perimeter[perimeter.length-1]['x'];
+    p2['y'] = perimeter[perimeter.length-1]['y'];
+    p3['x'] = x;
+    p3['y'] = y;
+
+    for(var i=0; i<perimeter.length-1; i++){
+        p0['x'] = perimeter[i]['x'];
+        p0['y'] = perimeter[i]['y'];
+        p1['x'] = perimeter[i+1]['x'];
+        p1['y'] = perimeter[i+1]['y'];
+        if(p1['x'] == p2['x'] && p1['y'] == p2['y']){ continue; }
+        if(p0['x'] == p3['x'] && p0['y'] == p3['y']){ continue; }
+        if(line_intersects(p0,p1,p2,p3)==true){
+            return true;
+        }
+    }
+    return false;
+}
+
+function point_it(event) {
+    if(complete){
+        alert('perimeter already created: '+JSON.stringify(perimeter));
+        return false;
+    }
+    var rect, x, y;
+
+    if(event.ctrlKey){
+        x = perimeter[0]['x'];
+        y = perimeter[0]['y'];
+        if(check_intersect(x,y)){
+            alert('The line you are drowing intersect another line');
+            return false;
+        }
+        draw(true);
+        alert(JSON.stringify(perimeter));
+    } else {
+        rect = canvas.getBoundingClientRect();
+        x = event.clientX - rect.left;
+        y = event.clientY - rect.top;
+        if(check_intersect(x,y)){
+            alert('The line you are drowing intersect another line');
+            return false;
+        }
+        perimeter.push({'x':x,'y':y});
+        draw(false);
+    }
+}
+
+function start(with_draw) {
+    var img = new Image();
+    img.src = canvas.getAttribute('data-imgsrc');
+
+    img.onload = function(){
+        ctx = canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+        if(with_draw == true){
+            draw(false);
+        }
+    }
+}
